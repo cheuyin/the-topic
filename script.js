21aba2324287678676a7021ba506567b54f10102
Yin <yinstanleycheung@gmail.com>
2022-07-18 13:48:22 -0700

    Add color picker for text color

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const topicInput = document.querySelector("#the-topic");
 const htmlBody = document.querySelector("body");
+const labels = document.querySelectorAll("label");
 
 const bgColor = document.querySelector("#bg-color");
 bgColor.addEventListener("change", function() {
   htmlBody.style.backgroundColor = this.value;
+})
+
+const textColor = document.querySelector("#text-color");
+textColor.addEventListener("change", function() {
+  topicInput.style.color = this.value;
+  labels.forEach(label => {
+    label.style.color = this.value;
+  })
 })

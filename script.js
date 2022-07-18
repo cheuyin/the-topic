645f5ecffce06f35ff2408d7cb7adc0fa03d3812
Yin <yinstanleycheung@gmail.com>
2022-07-18 13:55:37 -0700

    Dynamically change tab title to topic

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const topicInput = document.querySelector("#the-topic");
 const htmlBody = document.querySelector("body");
 const labels = document.querySelectorAll("label");
 
+topicInput.addEventListener("change", function() {
+  document.title = this.value;
+})
+
 const bgColor = document.querySelector("#bg-color");
 bgColor.addEventListener("change", function() {
   htmlBody.style.backgroundColor = this.value;
